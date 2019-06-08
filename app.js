66d2cf15fa95119f78febe338224b57a9f5775a4
Alex Muramoto <amuramoto@apache.org>
2019-06-07 17:22:28 -0700

    Switch to places api

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ import {TripsLayer} from '@deck.gl/geo-layers';
 
 // Set your Google Maps API key here or via environment variable
 const GOOGLE_MAPS_API_KEY = process.env.GoogleMapsAPIKey; // eslint-disable-line
-const GOOGLE_MAPS_API_URL = `https://maps.googleapis.com/maps/api/js?key=${GOOGLE_MAPS_API_KEY}`;
+const GOOGLE_MAPS_API_URL = `https://maps.googleapis.com/maps/api/js?key=${GOOGLE_MAPS_API_KEY}&libraries=places`;
+const MAP_CENTER = {lat: 41.954027649, lng: -87.763399032};
 const MAP_STYLES = [
   {
     "featureType": "administrative.land_parcel",
@@ -203,14 +204,28 @@ const MAP_STYLES = [
   }
 ];
 
-async function getTrips() {
-  // source: NYC Open Data 
-  // https://data.cityofnewyork.us/Environment/2015-Street-Tree-Census-Tree-Data/pi5s-9p35
-  const QUERY = '$where=within_circle(pickup_centroid_location,41.932875,-87.761911,1500) AND within_circle(dropoff_centroid_location,41.932875,-87.761911,1500) AND trip_start_timestamp!=trip_end_timestamp';
-  const TRIPS_URL = 'https://data.cityofchicago.org/resource/wrvz-psew.json?$limit=200&' + QUERY;
-  let trips = await fetch(TRIPS_URL)
-  trips = await trips.json();
-  trips = await formatTrips(trips);
+async function getTrips(map) {
+  const PLACES_SERVICE = new google.maps.places.PlacesService(map);
+  const OPTIONS = {
+    location: new google.maps.LatLng(MAP_CENTER),
+    radius: '2000',
+    type: ['restaurant']
+  };
+  let places_request = new Promise((resolve, reject) => {
+    PLACES_SERVICE.nearbySearch(OPTIONS, (res, status) => {
+      if (status !== google.maps.places.PlacesServiceStatus.OK) {
+        reject(status);
+      }
+      resolve(res)
+    });
+  });
+  let trips = await places_request;
+  const START = `${MAP_CENTER.lat},${MAP_CENTER.lng}`;
+  trips = trips.map(async(trip) => {
+    const END = `${trip.geometry.location.lat()},${trip.geometry.location.lng()}`;
+    let directions = await getDirections(START, END);
+  });
+  console.log(trips)
   return trips;
 }
 
@@ -226,7 +241,7 @@ async function formatTrips (trips) {
     let end_time = new Date(trip.trip_end_timestamp).getTime();
     let pickup_coords = [trip.pickup_centroid_latitude, trip.pickup_centroid_longitude];
     let dropoff_coords = [trip.dropoff_centroid_latitude, trip.dropoff_centroid_longitude]
-    // let waypoints = await getDirections(pickup_coords, dropoff_coords);
+    let waypoints = await getDirections(pickup_coords, dropoff_coords);
     return {
       'waypoints': [
         {
@@ -244,21 +259,11 @@ async function formatTrips (trips) {
   return trips;
 }
 
-async function getDirections (start_coords, end_coords) {
-  const directionsService = new google.maps.DirectionsService();
-  const request = {
-    origin: new google.maps.LatLng(start_coords[0], start_coords[1]),
-    destination: new google.maps.LatLng(end_coords[0], end_coords[1]),
-    travelMode: 'DRIVING'
-  };
-  let directions =  new Promise((resolve, reject) => {
-    directionsService.route(request, (response, status) => {
-      if (status === 'OK') {
-        resolve(response);
-      }
-      reject(status);
-    });
-  });
+async function getDirections (start, end) {
+  let request = fetch(`http://localhost:1337/directions?start=${start}&end=${end}`)
+  let response = await request;
+  response = await response.json();
+  let directions;
   return directions;
 }
 
@@ -275,170 +280,169 @@ function loadScript(url) {
 
 loadScript(GOOGLE_MAPS_API_URL).then(() => {
   const map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 41.954027649, lng: -87.763399032},
+    center: MAP_CENTER,
     zoom: 14,
     styles: MAP_STYLES
   });
-
   const directionsService = new google.maps.DirectionsService();
 
-  const TAXI_TRIPS = getTrips(directionsService);
-  const overlay = new GoogleMapsOverlay({
-    layers: [
-      new TripsLayer({
-        id: 'trips-layer',
-        data: TAXI_TRIPS,
-        getPath: d => d.waypoints.map(p => p.coords),
-        getTimestamps: d => d.waypoints.map(p => p.timestamp),
-        getColor: [253, 128, 93],
-        opacity: 0.8,
-        widthMinPixels: 5,
-        rounded: true,
-        trailLength: 200,
-        currentTime: 150
-      })
-    ]
-  });
-  overlay.setMap(map);
+  const trips = getTrips(map);
+  // const overlay = new GoogleMapsOverlay({
+  //   layers: [
+  //     new TripsLayer({
+  //       id: 'trips-layer',
+  //       data: TAXI_TRIPS,
+  //       getPath: d => d.waypoints.map(p => p.coords),
+  //       getTimestamps: d => d.waypoints.map(p => p.timestamp),
+  //       getColor: [253, 128, 93],
+  //       opacity: 0.8,
+  //       widthMinPixels: 5,
+  //       rounded: true,
+  //       trailLength: 200,
+  //       currentTime: 150
+  //     })
+  //   ]
+  // });
+  // overlay.setMap(map);
 });
 
 
-let test = [
-{
-waypoints: [
-{
-coordinates: [
--122.39079879999997,
-37.7664413
-],
-timestamp: 1554772579000
-},
-{
-coordinates: [
--122.3908298,
-37.7667706
-],
-timestamp: 1554772579009
-},
-{
-coordinates: [
--122.39271759999997,
-37.7667484
-],
-timestamp: 1554772579054
-},
-{
-coordinates: [
--122.3951341,
-37.7665964
-],
-timestamp: 1554772579092
-},
-{
-coordinates: [
--122.409425,
-37.7779834
-],
-timestamp: 1554772579345
-},
-{
-coordinates: [
--122.41318080000002,
-37.7750068
-],
-timestamp: 1554772579402
-},
-{
-coordinates: [
--122.41619750000001,
-37.7774034
-],
-timestamp: 1554772579462
-},
-{
-coordinates: [
--122.42135359999997,
-37.7770974
-],
-timestamp: 1554772579563
-},
-{
-coordinates: [
--122.42620490000002,
-37.8010553
-],
-timestamp: 1554772579880
-},
-{
-coordinates: [
--122.44484019999999,
-37.7989071
-],
-timestamp: 1554772580070
-},
-{
-coordinates: [
--122.4493488,
-37.801993
-],
-timestamp: 1554772580117
-},
-{
-coordinates: [
--122.44985459999998,
-37.8024803
-],
-timestamp: 1554772580120
-},
-{
-coordinates: [
--122.45090290000002,
-37.8033639
-],
-timestamp: 1554772580127
-},
-{
-coordinates: [
--122.45116330000002,
-37.8034643
-],
-timestamp: 1554772580130
-},
-{
-coordinates: [
--122.44840979999998,
-37.8046164
-],
-timestamp: 1554772580166
-},
-{
-coordinates: [
--122.44826899999998,
-37.8045327
-],
-timestamp: 1554772580176
-},
-{
-coordinates: [
--122.44827479999998,
-37.8044851
-],
-timestamp: 1554772580181
-},
-{
-coordinates: [
--122.44846849999999,
-37.8043839
-],
-timestamp: 1554772580186
-},
-{
-coordinates: [
--122.44856720000001,
-37.8040182
-],
-timestamp: 1554772580200
-}
-]
-}
-]
+// let test = [
+// {
+// waypoints: [
+// {
+// coordinates: [
+// -122.39079879999997,
+// 37.7664413
+// ],
+// timestamp: 1554772579000
+// },
+// {
+// coordinates: [
+// -122.3908298,
+// 37.7667706
+// ],
+// timestamp: 1554772579009
+// },
+// {
+// coordinates: [
+// -122.39271759999997,
+// 37.7667484
+// ],
+// timestamp: 1554772579054
+// },
+// {
+// coordinates: [
+// -122.3951341,
+// 37.7665964
+// ],
+// timestamp: 1554772579092
+// },
+// {
+// coordinates: [
+// -122.409425,
+// 37.7779834
+// ],
+// timestamp: 1554772579345
+// },
+// {
+// coordinates: [
+// -122.41318080000002,
+// 37.7750068
+// ],
+// timestamp: 1554772579402
+// },
+// {
+// coordinates: [
+// -122.41619750000001,
+// 37.7774034
+// ],
+// timestamp: 1554772579462
+// },
+// {
+// coordinates: [
+// -122.42135359999997,
+// 37.7770974
+// ],
+// timestamp: 1554772579563
+// },
+// {
+// coordinates: [
+// -122.42620490000002,
+// 37.8010553
+// ],
+// timestamp: 1554772579880
+// },
+// {
+// coordinates: [
+// -122.44484019999999,
+// 37.7989071
+// ],
+// timestamp: 1554772580070
+// },
+// {
+// coordinates: [
+// -122.4493488,
+// 37.801993
+// ],
+// timestamp: 1554772580117
+// },
+// {
+// coordinates: [
+// -122.44985459999998,
+// 37.8024803
+// ],
+// timestamp: 1554772580120
+// },
+// {
+// coordinates: [
+// -122.45090290000002,
+// 37.8033639
+// ],
+// timestamp: 1554772580127
+// },
+// {
+// coordinates: [
+// -122.45116330000002,
+// 37.8034643
+// ],
+// timestamp: 1554772580130
+// },
+// {
+// coordinates: [
+// -122.44840979999998,
+// 37.8046164
+// ],
+// timestamp: 1554772580166
+// },
+// {
+// coordinates: [
+// -122.44826899999998,
+// 37.8045327
+// ],
+// timestamp: 1554772580176
+// },
+// {
+// coordinates: [
+// -122.44827479999998,
+// 37.8044851
+// ],
+// timestamp: 1554772580181
+// },
+// {
+// coordinates: [
+// -122.44846849999999,
+// 37.8043839
+// ],
+// timestamp: 1554772580186
+// },
+// {
+// coordinates: [
+// -122.44856720000001,
+// 37.8040182
+// ],
+// timestamp: 1554772580200
+// }
+// ]
+// }
+// ]
--- server.js
+++ server.js
@@ -1,9 +1,52 @@
+const googleMapsClient = require('@google/maps').createClient({
+  key: process.env.GoogleMapsAPIKey,
+  Promise: Promise
+});
 const express = require('express');
 const app = express();
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 1337);
 
 app.use(express.static(__dirname + '/dist'));
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
+
+app.get('/directions', async(req, res) => {
+  const START = req.query.start.split(',');
+  const END = req.query.end.split(',');
+  const OPTIONS = {
+    origin: [START[0], START[1]],
+    destination: [END[0], END[1]],
+    mode: 'driving'
+  }
+  let request = googleMapsClient.directions(OPTIONS).asPromise();
+  let result = await request;
+  result = result.json.routes[0].legs[0];
+  let response = {
+    duration: result.duration.value,
+    waypoints: buildWaypoints(result.start_location, result.end_location, result.steps)
+  }
+ 
+});
+
+function buildWaypoints(start_coords, end_coords, steps) {
+  console.log(steps)
+  const directions = {
+    waypoints: []
+  };
+  
+  directions.waypoints.push({
+    coords: [start_coords.lng, start_coords.lat],
+    timestamp: 0
+  });
+
+  steps.forEach((step, index) => {
+    directions.waypoints.push({
+      coords: [step.end_location.lng, step.end_location.lat],
+      timestamp: 
+    });    
+  });
+  
+console.log(directions)
+}
